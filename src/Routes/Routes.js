22ab138acb637835c3be71940cf289d326ecea49
DESKTOP-S6IR4FG\nolu8 <noh8085@gmail.com>
2021-07-14 00:18:01 +0900

    redirect 추가

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Portfolio from '../pages/Portfolio';
 import Profile from '../pages/Profile';
 
@@ -7,8 +7,9 @@ const Routes = () => {
   return (
     <div>
       <Switch>
-        <Route exact path={['/profile', '/']} render={(props) => <Profile {...props} />} />
+        <Route exact path={'/profile'} render={(props) => <Profile {...props} />} />
         <Route exact path={'/portfolio'} render={(props) => <Portfolio {...props} />} />
+        <Redirect from="/" to="/profile" />
       </Switch>
     </div>
   );
